4fa460c1e27d8e79d099c36bec1e2f271f26ae4a
bayaman <bayaman23m@mail.ru>
2023-04-24 20:10:42 +0600

    api

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import Modal from "../components/Modal";
 import { useCommentContext } from "../contexts/CommentsContext";
-import { Avatar } from "@mui/material";
 
 function Comments() {
   const { comments, getComments } = useCommentContext();
